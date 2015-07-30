8dc0a10cd5a2c1d12a9a8d6f144517665edfbb7f
Raz <razgoldin@gmail.com>
2015-07-30 18:55:34 -0400

    shifting for Digital Ocean

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 //Set Express as my Http App Server
 var express = require("express");
 var app = express();
-var port = (process.env.PORT || 3000);
+var port = (process.env.PORT || 5555);
 
 //set the app to render from /public
 app.use(express.static(__dirname + '/public'));
